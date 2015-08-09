c3bb2c0eedf9ea88c73d151cec3aa430f5e28a01
kandros <kandros5591@gmail.com>
2015-08-09 13:25:38 +0200

    add routing

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -4,3 +4,10 @@ Router.route('/', {
   controller: 'HomeController',
   where: 'client'
 });
+
+Router.route('/cars/create', {
+  name: 'createCar',
+  controller: 'CarsController',
+  action: 'create',
+  where: 'client'
+});
