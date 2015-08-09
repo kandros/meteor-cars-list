981fa390c7c3bfe1042eb6298352a87ac38914fb
kandros <kandros5591@gmail.com>
2015-08-09 13:42:29 +0200

    add bootstrap styles to main template

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -1,7 +1,13 @@
+Router.configure({
+  layoutTemplate: 'MasterLayout',
+  loadingTemplate: 'Loading',
+  notFoundTemplate: 'NotFound'
+});
 
 Router.route('/', {
   name: 'home',
   controller: 'HomeController',
+  action: 'action',
   where: 'client'
 });
 
